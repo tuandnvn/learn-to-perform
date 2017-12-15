Fix a stupid bug (I used std instead of variance in the formulat to generate action with np.random.multivariate_normal

rl/action_learner.py
@@ -14,10 +14,16 @@ from gym.wrappers import TimeLimit
 
 def random_action(state, policy_estimator, no_of_actions = 1, verbose = False, session = None):
     action_means, action_stds = policy_estimator.predict(state, sess = session)
+
+    variances = action_stds ** 2
                     
-    actions = np.random.multivariate_normal(action_means,np.diag(action_stds), size = no_of_actions)
+    actions = np.random.multivariate_normal(action_means,np.diag(variances), size = no_of_actions)
 
     return action_means, action_stds, actions
+
+
+def epsilon_greedy_action( state, policy_estimator, no_of_actions = 1, verbose = False, session = None):
+    pass
 
 # def best_n_random_action(n):
 #     def best_random_action (state, policy_estimator, verbose = False):
@@ -165,6 +171,7 @@ class ActionLearner(object):
                         break
 
                     if verbose:
+                        print ((action_means, action_stds))
                         print ('best reward = %.2f' % best_reward)
 
                     # At this point, best_action corresponds to the best reward

rl/block_movement_env.py
@@ -461,7 +461,7 @@ class BlockMovementEnv(gym.Env):
             X, Y = np.meshgrid(x_range, y_range)
             z = mlab.bivariate_normal(X, Y, action_stds[0], action_stds[1],
                  action_means[0], action_means[1])
-            plt.contour(x_range, y_range, z, 4, alpha = 0.3)
+            plt.contour(x_range, y_range, z, 10, alpha = 0.3)
 
         plt.show()
 

rl/value_estimator.py
@@ -91,10 +91,11 @@ class PolicyEstimator():
                 inputs=state_expanded,
                 num_outputs=action_dimension,
                 activation_fn=None,
-                weights_initializer=tf.zeros_initializer))
+                weights_initializer=tf.random_uniform_initializer(minval=1.0/(5 * state_dimension), maxval=2.0/(5 * state_dimension))))
             
             """
             Using softplus so that the output would be > 0 but we also don't want 0
+            We look for sigma value ~ 0.2
             """
             self.sigma_layer = 0.5 * tf.squeeze(tf.contrib.layers.fully_connected(
                 inputs=state_expanded,